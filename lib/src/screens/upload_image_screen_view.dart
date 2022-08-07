b2b260cff524df94506d8c9f0fd0636f90f19927
Hooshyar <hooseyr@gmail.com>
2022-08-07 16:38:26 +0300

    upload image storage

--- lib/src/screens/upload_image_screen_view.dart
+++ lib/src/screens/upload_image_screen_view.dart
@@ -18,59 +18,101 @@ class _UploadImageScreenState extends State<UploadImageScreen> {
 
   XFile? _selectedImage;
   FirebaseStorage firebaseStorage = FirebaseStorage.instance;
+  bool isLoading = false;
+  String? downloadUrl;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            _selectedImage == null
-                ? Container(
-                    width: MediaQuery.of(context).size.width > 800 ? 4 : 2,
-                    child: Text('please select an image'),
-                  )
-                : Container(
-                    child: Image.file(File(_selectedImage!.path)),
-                  ),
-            ElevatedButton(
-                onPressed: () async {
-                  XFile? image =
-                      await _picker.pickImage(source: ImageSource.gallery);
-                  setState(() {
-                    _selectedImage = image;
-                  });
+      body: isLoading == true
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Container(
+              margin: EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _selectedImage == null
+                      ? Container(
+                          width: MediaQuery.of(context).size.width - 50,
+                          child: Text('please select an image'),
+                        )
+                      : Container(
+                          child: Image.file(File(_selectedImage!.path)),
+                        ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        XFile? image = await _picker.pickImage(
+                            source: ImageSource.gallery);
+                        setState(() {
+                          _selectedImage = image;
+                        });
 
-                  //TODO: pick an image
-                },
-                child: Text('select image')),
-            ElevatedButton(
-                onPressed: () {
-                  uploadImage(File(_selectedImage!.path));
-                  //TODO: upload the image to firebase storage
-                },
-                child: Text('upload the image')),
-          ],
-        ),
-      ),
+                        //TODO: pick an image
+                      },
+                      child: Text('select image')),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        uploadImage(File(_selectedImage!.path));
+                      },
+                      child: Text('upload the image')),
+                  downloadUrl == null
+                      ? Container()
+                      : SelectableText(downloadUrl!),
+                ],
+              ),
+            ),
     );
   }
 
-  uploadImage(File file) {
-    //TODO: userImages/something.jpg
+  uploadImage(File file) async {
+    //TODO: _loading when image i being uploaded
+    String fileName = DateTime.now().toString();
     final storageRef = FirebaseStorage.instance.ref();
+    //if we have a user you can do it like this
+    final imagesRef = storageRef.child("users/someUser/$fileName");
+    imagesRef.putFile(file).snapshotEvents.listen((taskSnapshot) async {
+      switch (taskSnapshot.state) {
+        case TaskState.running:
+          debugPrint('running');
+          break;
+        case TaskState.paused:
+          setState(() {
+            isLoading = false;
+          });
+          debugPrint('paused');
+          break;
+        case TaskState.success:
+          downloadUrl = await imagesRef.getDownloadURL();
 
-    // Create a child reference
-// imagesRef now points to "images"
-    final imagesRef = storageRef.child("userImages");
+          setState(() {
+            isLoading = false;
+          });
+          debugPrint('success, download url : $downloadUrl');
+          break;
+        case TaskState.canceled:
+          debugPrint('cancel');
+          setState(() {
+            isLoading = false;
+          });
+          break;
+        case TaskState.error:
+          setState(() {
+            isLoading = false;
+          });
+          debugPrint('error');
+          break;
+      }
+    });
 
-    imagesRef.putFile(file);
-
-// Child references can also take paths
-// spaceRef now points to "images/space.jpg
-// imagesRef still points to "images"
-    final spaceRef = storageRef.child("userImages/space.jpg");
+    // await imagesRef.getDownloadURL().then(
+    //       (value) => debugPrint(
+    //         value.toString(),
+    //       ),
+    //     );
   }
 }
